63c1ff34f1097b803b320ffce32091d2f30c6262
dhruvil-thakkar <dhruvil.thakkar@ecs.co.uk>
2020-02-26 15:37:12 +0000

    Adding vars for tfvars files

---
+++ variables.tf
@@ -0,0 +1,14 @@
+variable "region" {
+  type    = "string"
+  default = "us-east-1"
+}
+
+variable "aws_secret_key" {
+  type: string
+  default = " "
+}
+
+variable "aws_access_key" {
+  type: string
+  default = " "
+}
