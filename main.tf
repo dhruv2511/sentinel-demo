40b77ff54e7fba487b87aeb3aeea7419b69500b2
dhruvil-thakkar <dhruvil.thakkar@ecs.co.uk>
2020-02-26 15:33:52 +0000

    credentials to use tfvars

--- main.tf
+++ main.tf
@@ -8,9 +8,10 @@ terraform {
 }
 
 provider "aws" {
-  region                  = "us-east-1"
+  region                  = "${var.region}"
   profile                 = "default"
-  shared_credentials_file = "~/.aws/credentials"
+  access_key = "${var.aws_access_key}"
+  secret_key = "${var.aws_secret_key}"
 }
 
 
